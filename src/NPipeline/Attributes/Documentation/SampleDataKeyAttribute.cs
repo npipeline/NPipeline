e26fdc5b088189f8ef5264a18221bda991582732
Chris Jacques <cmjacques@gmail.com>
2026-04-10 21:54:35 +1000

    Delete SampleDataKeyAttribute.cs

--- src/NPipeline/Attributes/Documentation/SampleDataKeyAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace NPipeline.Attributes.Documentation;
-
-/// <summary>
-///    Marks a property as a key property that will be displayed in Studio's Node Detail Data table.
-/// </summary>
-[AttributeUsage(AttributeTargets.Property)]
-public sealed class SampleDataKeyAttribute : Attribute
-{
-}
